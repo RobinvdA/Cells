8411fb3ff29c9555bb07d01849fe39bffb48c048
Robin <robin.avoort@gmail.com>
2019-06-27 23:39:28 +0200

    Working on general flow

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<script src="../../../../../Users/Robin/AppData/Local/Temp/app.js"></script>
 <template>
     <v-app>
         <v-toolbar app>
@@ -20,23 +19,87 @@
 
         <v-content>
 
-            <game />
+            <v-container v-if="! name" grid-list-lg>
+                <v-layout>
+                    <v-flex>
+                        <v-card>
+                            <v-card-text>
+                                <v-text-field
+                                        v-model="typedName"
+                                        @click:append-outer="saveName"
+                                        :loading="savingName"
+                                        label="Nickname"
+                                        type="text"
+                                        append-outer-icon="save"
+                                ></v-text-field>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+
+            <lobby v-else-if="! game" :socket="socket"></lobby>
+
+            <game v-else :game="game"></game>
 
         </v-content>
     </v-app>
 </template>
 
 <script>
-    import Game from './components/Game'
+    import Lobby from './components/Lobby';
+    import Game from './components/Game';
 
     export default {
         components: {
+            Lobby,
             Game
         },
 
         data() {
             return {
+                socket: null,
 
+                game: null,
+
+                typedName: null,
+                name: null,
+
+                savingName: false
+            }
+        },
+
+        mounted() {
+            this.initSocket();
+
+            this.initSocketListeners();
+        },
+
+        methods: {
+            saveName() {
+                this.savingName = true;
+
+                this.socket.emit('save-name', this.typedName);
+            },
+
+            initSocketListeners() {
+                this.socket.on('name-saved', (name) => {
+                    this.name = name;
+
+                    this.savingName = false;
+                });
+
+                this.socket.on('joined-game', (game) => {
+                    this.game = game;
+                });
+
+                this.socket.on('left-game', () => {
+                    this.game = null;
+                });
+            },
+
+            initSocket() {
+                this.socket = new IO(`http://localhost:3000`);
             }
         }
     }
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,17 +1,32 @@
 <template>
 
-    <v-layout column align-center>
-        <v-flex my-3>
-            <v-btn @click="join" color="primary">
-                Join
-            </v-btn>
+    <v-layout row>
+        <v-flex>
+            <v-card>
+                <v-card-title class="headline">
+                    RobinvdA's game
+                </v-card-title>
+
+                <v-data-table :items="players" hide-headers hide-actions>
+                    <template v-slot:items="props">
+                        <td :class="props.item.color"></td>
+                        <td class="px-3">
+                            {{ props.item.name }}
+                        </td>
+                    </template>
+                </v-data-table>
+            </v-card>
         </v-flex>
 
-        <v-flex>
+        <v-flex shrink>
 
-            <v-card width="500px" height="400px">
+            <v-card width="500px">
 
-                <canvas ref="gameContainer" style="width:100%;height:100%">
+                <v-card-title class="title">
+                    Please wait...
+                </v-card-title>
+
+                <canvas ref="gameContainer" style="width:100%;height:400px">
 
                 </canvas>
 
@@ -26,48 +41,52 @@
     import Game from './../engine/game';
 
     export default {
+        props: {
+            socket: {
+                default: null
+            },
+
+            game: {
+                default: null
+            }
+        },
 
         data() {
             return {
-                game: null,
+                engine: null,
 
-                joining: false,
-                joined: false,
+                players: [],
 
-                player: {
-                    color: null
-                },
-
-                socket: null
+                running: false
             }
         },
 
         mounted() {
-            this.initSocket();
+
         },
 
         methods: {
             join() {
-                this.game = new Game(this.$refs.gameContainer, {
+                this.engine = new Game(this.$refs.gameContainer, {
                     cellSize: 30
                 });
 
-                this.game.onClick((x, y, cellX, cellY) => {
+                this.engine.onClick((x, y, cellX, cellY) => {
                     console.log('Click', cellX, cellY);
                 });
 
                 window.addEventListener('keydown', (event) => {
                     switch(event.keyCode) {
-                        case 37: return this.game.startMoving('left');
-                        case 38: return this.game.startMoving('up');
-                        case 39: return this.game.startMoving('right');
-                        case 40: return this.game.startMoving('down');
+                        case 37: return this.engine.startMoving('left');
+                        case 38: return this.engine.startMoving('up');
+                        case 39: return this.engine.startMoving('right');
+                        case 40: return this.engine.startMoving('down');
                     }
                 });
 
                 window.addEventListener('keyup', (event) => {
                     switch(event.keyCode) {
-                        case 37: case 38: case 39: case 40: this.game.stopMoving();
+                        case 37: case 38: case 39: case 40: this.engine.stopMoving();
                     }
                 });
 
@@ -75,41 +94,40 @@
             },
 
             run() {
-                if (! this.game) return;
+                if (! this.engine) return;
 
-                this.game.run();
+                this.engine.run();
             },
 
             stop() {
-                if (! this.game) return;
+                if (! this.engine) return;
 
-                this.game.stop();
+                this.engine.stop();
             },
 
             loadPlayer() {
                 this.socket.emit('join');
 
-                this.socket.on('player', (player) => {
-                    this.player = player;
-                    this.joined = true;
-                });
-
-                this.socket.on('game-full', () => {
-                    this.joined = false;
-                });
-
                 this.socket.on('grid', (grid) => {
-                    this.game.init(grid).center();
+                    this.engine.init(grid).center();
                 });
 
                 this.socket.on('game-start', () => {
+                    this.running = true;
+
                     this.run();
                 });
-            },
 
-            initSocket() {
-                this.socket = new IO(`http://localhost:3000`);
+                this.socket.on('game-state', (grid) => {
+                    this.engine.init(grid);
+                });
             }
+        },
+
+        beforeDestroy() {
+            this.socket.removeListener('grid');
+            this.socket.removeListener('game-start');
+            this.socket.removeListener('game-state');
         }
 
     }
